54e77ec8e9fe7679478843f1ab3ae3f312965fa2
aycact <CT06N0159@actvn.edu.vn>
2024-08-09 11:55:38 +0700

    updated readme, db rename tv into db

--- db.sql
+++ db.sql
@@ -2,15 +2,14 @@ set allow_experimental_object_type = 1;
 create database tuvi;
 use tuvi;
 create table age(
-id UUID default generateUUIDv7() primary key,
+url String not null primary key,
 year Int16 not null default 0,
 s_age String not null,
 b_year Int16 not null default 0,
 gender bool not null
-)engine = MergeTree
-order by (id);
+)engine = MergeTree;
 create table info(
-id UUID default generateUUIDv7() primary key,
+url String not null primary key,
 b_year Int16 not null default 0,
 l_age Int16 not null default 0,
 van_nien String not null,
@@ -18,21 +17,42 @@ sao_han String not null,
 kim_lau String not null,
 tam_tai String not null,
 hoang_oc String not null
-)engine = MergeTree
-order by (id);
+)engine = MergeTree;
 create table destiny(
-id UUID default generateUUIDv7() primary key,
+url String not null primary key,
 kim_lau String not null,
 tam_tai String not null,
 hoang_oc String not null
-)engine = MergeTree
-order by (id);
+)engine = MergeTree;
 create table tv(
 url String not null primary key,
-age_id UUID not null,
-info_id UUID not null,
-destiny_id UUID not null,
 summary String null,
 fengshui JSON not null
 )engine = MergeTree;
-create view if not exists tv_view as select tv.url, age.year, age.s_age, age.b_year, age.gender, info.l_age, info.van_nien, info.sao_han, info.kim_lau, info.tam_tai, info.hoang_oc, destiny.kim_lau, destiny.tam_tai, destiny.hoang_oc, tv.summary, tv.fengshui from tv join age on tv.age_id = age.id join info on tv.info_id = info.id join destiny on tv.destiny_id = destiny.id;
+CREATE VIEW IF NOT EXISTS tv_view AS
+SELECT 
+    tv.url, 
+    age.year AS year, 
+    age.s_age AS s_age, 
+    age.b_year AS b_year, 
+    age.gender AS gender, 
+    info.l_age AS l_age, 
+    info.van_nien AS van_nien, 
+    info.sao_han AS sao_han, 
+    info.kim_lau AS info_kim_lau, 
+    info.tam_tai AS info_tam_tai, 
+    info.hoang_oc AS info_hoang_oc, 
+    destiny.kim_lau AS destiny_kim_lau, 
+    destiny.tam_tai AS destiny_tam_tai, 
+    destiny.hoang_oc AS destiny_hoang_oc, 
+    tv.summary, 
+    tv.fengshui
+FROM 
+    tv 
+JOIN 
+    age ON tv.url = age.url
+JOIN
+    info ON tv.url = info.url
+JOIN
+    destiny ON tv.url = destiny.url;
+
